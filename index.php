275560fe705c83c4734e98d73c729c9c8972a74e
Mr_fortuna <108610775+mrf0rtuna4@users.noreply.github.com>
2023-01-21 09:36:31 +0300

    hm php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+echo "<!DOCTYPE html>\n";
+echo "<!--все на русском точно-точно!)-->\n";
+echo "<html lang=\"ru\">\n";
+echo "    <head>\n";
+echo "     <meta charset=\"UTF-8\">\n";
+echo "     <meta property=\"og:site_name\" content=\"Mr_Fortuna\">\n";
+echo "     <link rel=\"stylesheet\" href=\"assets/css/styles.css\" type=\"text/css\"/>\n";
+echo "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n";
+echo "<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n";
+echo "<link href=\"https://fonts.googleapis.com/css2?family=Rubik+Bubbles&display=swap\" rel=\"stylesheet\">\n";
+echo "    <title>Info for Mr_Fortuna</title>\n";
+echo "    </head>\n";
+echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+echo "<body>\n";
+echo "      <header>\n";
+echo "        <a href=\"index.html\"><p class=\"back\"> Главная </p></a>\n";
+echo "        <img src=\"https://cdn.discordapp.com/avatars/702443229924032512/62a01b23d407da3070ee89a21b573021.png?size=4096\" width=\"100px\" height=\"100px\">\n";
+echo "        <a href=\"https://github.com/mrf0rtuna4\"><img class=\"git\" src=\"assets/images/25231.png\" alt=\"\"></a> <br>\n";
+echo "        <a href=\"https://discord.gg/NFRtqcu7fq\"><img class=\"hrl\" src=\"assets/images/nl.jpg\" alt=\"\"></a>\n";
+echo "<a href=\"discord.html\"><img class=\"ds\" src=\"assets/images/Discord.png\" alt=\"\"></a>  \n";
+echo "\n";
+echo "     </header>\n";
+echo "   <h1> Mr_Fortuna </h1> \n";
+echo "<p> Пока вы спите, мы двигаемся вперед </p>\n";
+echo "</body>\n";
+echo "\n";
+echo "</html>\n";
+?>
