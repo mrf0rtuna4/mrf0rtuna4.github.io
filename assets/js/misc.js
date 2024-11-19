61986293fc4d9e35b42dd0057bcbd0fb75691c92
mrf0rtuna4 <majned90@gmail.com>
2024-11-19 20:11:44 +0300

    scary updates

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,4 +1,4 @@
-document.addEventListener('scroll', function() {
+document.addEventListener('scroll', function () {
     const skillsSection = document.querySelector('#skills');
     const skillsPosition = skillsSection.getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 1.3;
@@ -18,3 +18,47 @@ projectCards.forEach((card) => {
         card.classList.remove('animate__pulse');
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // const header = document.querySelector('header');
+    // const avatarContainer = document.querySelector('.avatar-container');
+
+    // window.addEventListener('scroll', () => {
+    //     if (window.scrollY > 50) {
+    //         header.classList.add('scrolled');
+    //     } else {
+    //         header.classList.remove('scrolled');
+    //     }
+    // });
+
+
+    const themeSwitch = document.querySelector('.theme-switch');
+    const body = document.body;
+
+    themeSwitch.addEventListener('click', () => {
+        body.classList.toggle('light-mode');
+        body.classList.toggle('dark-mode');
+    });
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const header = document.querySelector('header');
+//     const aboutSection = document.querySelector('#about');
+//     const avatarContainer = document.querySelector('.avatar-container');
+
+//     const onScroll = () => {
+//         const aboutTop = aboutSection.getBoundingClientRect().top;
+//         const threshold = 100;
+
+//         if (aboutTop <= threshold) {
+//             header.classList.add('scrolled');
+//             aboutSection.querySelector('.avatar-container').style.opacity = '0';
+//         } else {
+//             header.classList.remove('scrolled');
+//             aboutSection.querySelector('.avatar-container').style.opacity = '1';
+//         }
+//     };
+
+//     window.addEventListener('scroll', onScroll);
+// });
